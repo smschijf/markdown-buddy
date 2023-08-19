fb86dfc8d3652b7612bd16b6ef13b7618b827d2d
smschijf <210578@student.glu.nl>
2023-08-19 13:56:30 +0200

    expanded test note with metadata and list

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import Test from "../notes/test.mdx";
 export default function Home() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0">
-      <Test />
+      <article>
+        <Test />
+      </article>
     </main>
   );
 }
